01e9d6b6ae8978e026b09c109f9ede11b3c2367a
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-09-29 11:16:09 +0200

    init kadelmia

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Kameldia_p2p
+# Kademlia Node
+Ce projet est une implémentation d'un nœud Kademlia en Python. Kademlia est un protocole de réseau décentralisé utilisé pour la recherche décentralisée et le stockage de données dans un réseau pair à pair (P2P). Ce nœud Kademlia peut être utilisé pour créer et rejoindre un réseau Kademlia, stocker des données de manière décentralisée et effectuer des recherches sur le réseau.
+
+# Configuration Requise
+Pour exécuter ce code, vous aurez besoin de Python 3.x installé sur votre système. De plus, vous devez installer les dépendances requises en utilisant le gestionnaire de paquets pip. Les dépendances peuvent être installées en exécutant la commande suivante :
+
+```bash
+pip install kademlia
+```
+# Utilisation
+Pour utiliser ce nœud Kademlia, suivez ces étapes :
+1. Clonez ce dépôt sur votre système.
+2. Assurez-vous que les dépendances requises sont installées en utilisant la commande mentionnée dans la section Configuration Requise.
+3. Exécutez le fichier main.py en utilisant Python 3.x :
+
+```bash
+python main.py
+```
+1. Vous serez invité à répondre à quelques questions sur la configuration du nœud Kademlia, telles que si c'est la première nœud du réseau, le port d'écoute, et les informations de la première nœud si ce n'est pas la première.
+
+2. Une fois configuré, le nœud Kademlia sera opérationnel et vous pourrez effectuer des opérations telles que le stockage et la récupération de données sur le réseau.
---
+++ kademlia_node.py
@@ -0,0 +1,78 @@
+import logging
+import asyncio
+import threading
+from kademlia.network import Server
+
+# Initialize the logging configuration
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+log = logging.getLogger('kademlia')
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
+
+async def run(port, bootstrap_ip, bootstrap_port):
+    """
+    Run the Kademlia node and handle user input.
+    """
+    # Create a node and start listening on the specified port
+    node = Server()
+    await node.listen(port)
+
+    # Bootstrap the node by connecting to the specified IP and port
+    await node.bootstrap([(bootstrap_ip, bootstrap_port)])
+    
+    while True:
+        a = input("Option 1 = set  2 = get:")
+        if a == '1':
+            await node.set("my-key", "my awesome value")
+        elif a == '2':
+            # get the value associated with "my-key" from the network
+            result = await node.get("my-key")
+            print(result)
+        else:
+            print("Bad input, try again")
+
+def user_input_thread(port, bootstrap_ip, bootstrap_port):
+    """
+    Create and run the user input thread.
+    """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(run(port, bootstrap_ip, bootstrap_port))
+
+while True:
+    firstNode = input("Êtes-vous la première node du réseau ? (Y ou n)\n").strip().upper()
+    if firstNode == "Y" or firstNode == "N":
+        break
+    else:
+        print("Mauvaise entrée, seulement Y ou n")
+port = int(input("Veuillez entrer le port sur lequel écouter : "))
+
+if firstNode == "N":
+    bootstrap_ip = input("Veuillez entrer l'adresse IP de la première node : ")
+    bootstrap_port = int(input("Veuillez entrer le port de la première node : "))
+else:
+    bootstrap_ip = None
+    bootstrap_port = None
+
+if firstNode == "Y":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    node = Server()
+    loop.run_until_complete(node.listen(port))
+else:
+    input_thread = threading.Thread(target=user_input_thread, args=(port, bootstrap_ip, bootstrap_port))
+    input_thread.daemon = True
+    input_thread.start()
+
+try:
+    if firstNode == "Y":
+        loop.run_forever()
+    else:
+        input_thread.join()
+except KeyboardInterrupt:
+    pass
+finally:
+    # Ajoutez ici tout code de nettoyage nécessaire
+    pass
